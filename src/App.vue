b34bf431f50a3ab7adb388c9013d3db932d31954
Josh Vardey <jocelynvardey@gmail.com>
2018-04-25 16:33:55 +0200

    restart

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    
     <router-view/>
+
+    <div id="nav">
+      <a target="_blank" href="http://www.joshvardey.com/">Journalist</a> |
+      <router-link to="/about">Coder</router-link>
+    </div>
   </div>
 </template>
 
